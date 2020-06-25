ea79b55b0617a5b29d18cf7e4e5f88b57b008f8a
iamdempa <banukajananathjayarathna@gmail.com>
2020-06-25 22:45:56 +0530

    test

---
+++ build/precache-manifest.b55137d9fb40fc4e30bb3afbc2dba3e4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5804fd03ed17cdaee5b1f5df001c87a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3545625b16f0c4f4db14",
+    "url": "/static/css/2.b450f4ca.chunk.css"
+  },
+  {
+    "revision": "5e3a302cd272f8399355",
+    "url": "/static/css/main.88e9529e.chunk.css"
+  },
+  {
+    "revision": "3545625b16f0c4f4db14",
+    "url": "/static/js/2.ac49b663.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ac49b663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e3a302cd272f8399355",
+    "url": "/static/js/main.44bcb7eb.chunk.js"
+  },
+  {
+    "revision": "ed58b726c534e7c02f2c",
+    "url": "/static/js/runtime-main.60eded72.js"
+  },
+  {
+    "revision": "7d6af431224cec59507680a796e073fc",
+    "url": "/static/media/aboutme.7d6af431.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "efdba04681a3215e786c0e996e4bc780",
+    "url": "/static/media/header-background2.efdba046.jpg"
+  }
+]);
